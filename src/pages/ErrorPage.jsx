37cc89b414786e1cf9d2fd1d74b02ea95a9c167c
egamovj <egamovj90@gmail.com>
2024-07-31 17:18:45 +0500

    error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,38 @@
+/* eslint-disable no-unused-vars */
+import { Link, useRouteError } from "react-router-dom";
 
 function ErrorPage() {
+  const error = useRouteError();
+
+  if (error.status == 404) {
+    return (
+      <div className="error-container container">
+        <div>
+          <h3>
+            404 Error: Page not found. But don`t worry, our developers are on a
+            treasure hunt to recover it.
+          </h3>
+          <Link to={"/"} className="btn">
+            Go to home
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      
+    <div className="error-container container">
+      <div>
+        <h3>
+          Oops! Looks like our web page did a somersault and landed in a digital
+          rabbit hole. We`re working on untangling the code. Hang tight!
+        </h3>
+        <Link to={"/"} className="btn">
+          Go to home
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ErrorPage
+export default ErrorPage;
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -2,7 +2,7 @@
 function Quiz() {
   return (
     <div>
-      
+      Quiz
     </div>
   )
 }
