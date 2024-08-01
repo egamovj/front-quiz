1b202d7e5e5b9f2c9cd555192c8d0284c9fc8985
egamovj <egamovj90@gmail.com>
2024-08-01 10:17:30 +0500

    dark mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,41 @@
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from "react";
+
+const toggleMode = () => {
+  return localStorage.getItem("darkMode") || "light";
+};
 
 function Navbar() {
+  const [theme, setTheme] = useState(toggleMode());
+
+  const handleThemeToggle = () => {
+    const newTheme = theme == "dark-mode" ? "light" : "dark-mode";
+
+    setTheme(newTheme);
+  };
+
+  useEffect(() => {
+    document.body.classList = "";
+    document.body.classList.add(theme);
+    localStorage.setItem("darkMode", theme);
+  }, [theme]);
+
   return (
-    <div>
-      Navbar
-    </div>
-  )
+    <header className="header">
+      <div className="header-container container">
+        <div></div>
+        <div>
+          <div className="dark-btn" onClick={handleThemeToggle}>
+            <input type="checkbox" />
+            <span>
+              <span></span>
+              <span></span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
